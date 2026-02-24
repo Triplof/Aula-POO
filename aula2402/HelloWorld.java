484d9970082dffbac5867bdcdc9faae3e89d4f4f
FELIPE FURLAN FILHO <felipe.filho004@cs.unicid.edu.br>
2026-02-24 20:45:01 -0300

    first commit

---
+++ aula2402/HelloWorld.java
@@ -0,0 +1,24 @@
+package aula2402;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 30;
+		int soma = (a + b);
+		
+		
+		System.out.println("Hello World");
+		System.out.println("Resultado..... " + soma);
+		
+		int soma2 = (soma + b);
+		
+		System.out.println("Resultado... " + soma2);
+		
+		
+		
+		
+		
+	}
+
+}
