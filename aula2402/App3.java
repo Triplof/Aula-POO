b9b7bed8c1c292a3c5bf1320527aa408da0a043e
FELIPE FURLAN FILHO <felipe.filho004@cs.unicid.edu.br>
2026-02-24 21:24:34 -0300

    App3 Arrumado

--- aula2402/App3.java
+++ aula2402/App3.java
@@ -3,8 +3,12 @@ package aula2402;
 public class App3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	int soma = 1;
+		for (int x = 1; x <=15; x+=2) {
+			soma = soma*x;
+		}
+			System.out.println("Resultado: " + soma);
 
+		}
+		
 	}
-
-}
