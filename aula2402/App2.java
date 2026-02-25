81d4acee9fa4714e6bf196be970181857100aecf
FELIPE FURLAN FILHO <felipe.filho004@cs.unicid.edu.br>
2026-02-24 21:13:04 -0300

    novo app

---
+++ aula2402/App2.java
@@ -0,0 +1,14 @@
+package aula2402;
+
+public class App2 {
+
+	public static void main(String[] args) {
+	int soma = 0;
+		for (int x = 1; x <=100; x++) {
+			soma += x;
+		}
+			System.out.println("Resultado: " + soma);
+
+		}
+		
+	}
