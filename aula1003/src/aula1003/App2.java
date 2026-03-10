d7dc86c0e107788babd43b83fad489e51cdb08fd
FELIPE FURLAN FILHO <felipe.filho004@cs.unicid.edu.br>
2026-03-10 20:07:14 -0300

    Aula 10 03

---
+++ aula1003/src/aula1003/App2.java
@@ -0,0 +1,16 @@
+package aula1003;
+
+public class App2 {
+
+	public static void main(String[] args) {
+		// vetor ou matriz
+		
+		String nome[] = new String[3];
+		nome[0] = "Jose";
+		nome[0] = "Ana Maria";
+		nome[0] = "Eduarda";
+		
+		
+	}
+
+}
