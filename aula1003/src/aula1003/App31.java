63467d3b5a0f17891a1d329fa6c536bfff14d35e
FELIPE FURLAN FILHO <felipe.filho004@cs.unicid.edu.br>
2026-03-10 20:40:26 -0300

    adicionado contador

--- aula1003/src/aula1003/App31.java
+++ aula1003/src/aula1003/App31.java
@@ -2,7 +2,7 @@ package aula1003;
 
 import java.util.Scanner;
 
-public class App3 {
+public class App31 {
 	
 	//Entre com um nr. e informe quantas vezes ele se repete
 	
@@ -17,12 +17,14 @@ public class App3 {
 		int contador = 0;
 				
 		int valor[] = {10,47,78,99,7,4,5,8,9,67,42,47,76,61,44,24,38};
-		int soma = 0;
+		
 		for(int i=0; i<valor.length; i++) {
-			 soma += valor[i];
+			 if (numero == valor[i]) {
+				 contador += 1;
+			 }
 			
 		}
-			System.out.println("Sua soma é: " + soma);
+			System.out.println("Seu numero aparece: " + contador);
 	}
 
 }
