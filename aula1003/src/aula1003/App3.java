4a2d5beb85c71fcbe4c362d684a2d9481329d839
FELIPE FURLAN FILHO <felipe.filho004@cs.unicid.edu.br>
2026-03-10 20:30:50 -0300

    aula 10 03

--- aula1003/src/aula1003/App3.java
+++ aula1003/src/aula1003/App3.java
@@ -4,10 +4,10 @@ public class App3 {
 	
 	// some os valores do vetor
 	public static void main(String[] args) {
-		int valor[] = {10,47,78,99,7};
+		int valor[] = {10,47,78,99,7,4,5,8,9,67,42,47,76,61,44,24,38};
 		int soma = 0;
-		for(int i=0; i<5; i++) {
-			 soma = soma+valor[i];
+		for(int i=0; i<valor.length; i++) {
+			 soma += valor[i];
 			
 		}
 			System.out.println("Sua soma é: " + soma);
