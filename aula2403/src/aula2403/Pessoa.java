2fd1829e58f2af5607298f9ed881ace6998ed709
FELIPE FURLAN FILHO <felipe.filho004@cs.unicid.edu.br>
2026-03-24 19:49:37 -0300

    aula 2403

---
+++ aula2403/src/aula2403/Pessoa.java
@@ -0,0 +1,9 @@
+package aula2403;
+
+public class Pessoa {
+	// atributos da classe >> caracteristicas
+	int idade;
+	String nome;
+	
+	
+}
---
+++ aula2403/src/aula2403/Principal.java
@@ -0,0 +1,18 @@
+package aula2403;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// chamo uma classe >> instanciar a classe
+		Pessoa p = new Pessoa();
+		int a = 10;
+		
+		System.out.println("Nome......:" + p.nome);
+		System.out.println("Idade.....:" + p.idade);
+		
+		
+	}
+
+}
